dbba68c623901b023322b0f78f7122d7f406b3c9
Arin yoga <arin.s@hotmail.de>
2024-10-26 19:47:04 +0200

    mok works

--- server/project/server.py
+++ server/project/server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Der Server läuft!"
+    return render_template("index.html")
 
 
 @app.route('/favicon.ico')
